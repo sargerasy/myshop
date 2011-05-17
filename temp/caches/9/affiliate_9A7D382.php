91daba0b99411df797ab516bb06bff077dcfc6a4
unknown <Administrator@.(none)>
2011-05-18 07:46:36 +0800

    ignore temp directory

--- temp/caches/9/affiliate_9A7D382.php
+++
@@ -1,5 +0,0 @@
-<?php exit;?>a:3:{s:8:"template";a:1:{i:0;s:45:"F:/xamp/xampp/htdocs/shop/data/affiliate.html";}s:7:"expires";i:1293587751;s:8:"maketime";i:1293584151;}<table width="100%"  border="0" cellspacing="0" cellpadding="0">
-  <tr>
-    <td align="center"><a href="http://localhost/shop/goods.php?u=6&id=24" target="_blank"><big>P806</big></a><br /><s>￥2400元</s> <font color = "#FF0000">￥2000元</font></td>
-  </tr>
-</table>
